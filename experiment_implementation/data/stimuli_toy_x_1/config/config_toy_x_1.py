fd49bc9df5c24a8e526b7556a7951108fdb1e809
theDebbister <deborahnoemie.jakobi@uzh.ch>
2024-05-06 17:10:35 +0200

    add full toy experiment

---
+++ experiment_implementation/data/stimuli_toy_x_1/config/config_toy_x_1.py
@@ -0,0 +1,74 @@
+
+# QUESTION_OPTION_BOXES ##########
+down = (224.95, 900.0, 1330.25, 1104.0)
+right = (819.6500000000001, 528.0, 1471.1, 864.0)
+up = (224.95, 300.0, 1330.25, 504.0)
+left = (88.2, 528.0, 739.65, 864.0)
+
+# QUESTION_CSV_PATHS ##########
+question_images_question_images_version_1_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_1\multipleye_comprehension_questions_toy_question_images_version_1_with_img_paths.csv"
+question_images_question_images_version_2_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_2\multipleye_comprehension_questions_toy_question_images_version_2_with_img_paths.csv"
+question_images_question_images_version_3_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_3\multipleye_comprehension_questions_toy_question_images_version_3_with_img_paths.csv"
+question_images_question_images_version_4_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_4\multipleye_comprehension_questions_toy_question_images_version_4_with_img_paths.csv"
+question_images_question_images_version_5_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_5\multipleye_comprehension_questions_toy_question_images_version_5_with_img_paths.csv"
+question_images_question_images_version_6_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_6\multipleye_comprehension_questions_toy_question_images_version_6_with_img_paths.csv"
+question_images_question_images_version_7_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_7\multipleye_comprehension_questions_toy_question_images_version_7_with_img_paths.csv"
+question_images_question_images_version_8_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_8\multipleye_comprehension_questions_toy_question_images_version_8_with_img_paths.csv"
+question_images_question_images_version_9_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_9\multipleye_comprehension_questions_toy_question_images_version_9_with_img_paths.csv"
+question_images_question_images_version_10_csv = "data/stimuli_toy_x_1/question_images_toy_x_1/question_images_version_10\multipleye_comprehension_questions_toy_question_images_version_10_with_img_paths.csv"
+question_images_question_images_version_1_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_1\multipleye_comprehension_questions_toy_aoi_question_images_version_1_with_img_paths.csv"
+question_images_question_images_version_2_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_2\multipleye_comprehension_questions_toy_aoi_question_images_version_2_with_img_paths.csv"
+question_images_question_images_version_3_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_3\multipleye_comprehension_questions_toy_aoi_question_images_version_3_with_img_paths.csv"
+question_images_question_images_version_4_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_4\multipleye_comprehension_questions_toy_aoi_question_images_version_4_with_img_paths.csv"
+question_images_question_images_version_5_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_5\multipleye_comprehension_questions_toy_aoi_question_images_version_5_with_img_paths.csv"
+question_images_question_images_version_6_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_6\multipleye_comprehension_questions_toy_aoi_question_images_version_6_with_img_paths.csv"
+question_images_question_images_version_7_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_7\multipleye_comprehension_questions_toy_aoi_question_images_version_7_with_img_paths.csv"
+question_images_question_images_version_8_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_8\multipleye_comprehension_questions_toy_aoi_question_images_version_8_with_img_paths.csv"
+question_images_question_images_version_9_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_9\multipleye_comprehension_questions_toy_aoi_question_images_version_9_with_img_paths.csv"
+question_images_question_images_version_10_aoi_csv = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/question_images_version_10\multipleye_comprehension_questions_toy_aoi_question_images_version_10_with_img_paths.csv"
+
+# PATHS ##########
+stimuli_images_csv = "data/stimuli_toy_x_1/multipleye_stimuli_experiment_toy_x_1_with_img_paths.csv"
+stimuli_images_aoi_csv = "data/stimuli_toy_x_1/multipleye_stimuli_experiment_toy_x_1_aoi_with_img_paths.csv"
+participant_instruction_csv = "data/stimuli_toy_x_1/multipleye_participant_instructions_toy_with_img_paths.csv"
+question_file_excel = "data/stimuli_toy_x_1/multipleye_comprehension_questions_toy.xlsx"
+participant_instruction_excel = "data/stimuli_toy_x_1/multipleye_participant_instructions_toy.xlsx"
+stimuli_file_excel = "data/stimuli_toy_x_1/multipleye_stimuli_experiment_toy.xlsx"
+
+# RATING_QUESTION_BOXES ##########
+option_1 = (107.8, 331.7, 733.8, 369.7)
+option_2 = (107.8, 438.7, 733.8, 476.7)
+option_3 = (107.8, 545.7, 733.8, 583.7)
+option_4 = (107.8, 652.7, 733.8, 690.7)
+option_5 = (107.8, 759.7, 733.8, 797.7)
+
+# EXPERIMENT ##########
+LANGUAGE = "toy"
+
+# IMAGE ##########
+FONT_SIZE = 28
+FONT = "fonts/JetBrainsMono-Regular.ttf"
+FGC = (0, 0, 0)
+IMAGE_BGC = (231, 230, 230)
+IMAGE_WIDTH_PX = 1565
+IMAGE_HEIGHT_PX = 1200
+MIN_MARGIN_LEFT_PX = 98
+MIN_MARGIN_RIGHT_PX = 90
+MIN_MARGIN_TOP_PX = 107
+MIN_MARGIN_BOTTOM_PX = 141
+IMAGE_SIZE_CM = (36.5, 28)
+MAX_CHARS_PER_LINE = 82
+
+# SCREEN ##########
+RESOLUTION = (2560, 1440)
+SCREEN_SIZE_CM = (59.7, 33.6)
+DISTANCE_CM = 60.0
+
+# DIRECTORIES ##########
+question_image_dir = "data/stimuli_toy_x_1/question_images_toy_x_1/"
+image_dir = "data/stimuli_toy_x_1/stimuli_images_toy_x_1/"
+aoi_dir = "data/stimuli_toy_x_1/aoi_stimuli_toy_x_1/"
+aoi_question_dir = "data/stimuli_toy_x_1/aoi_question_images_toy_x_1/"
+aoi_image_dir = "data/stimuli_toy_x_1/aoi_stimuli_images_toy_x_1/"
+other_screens_dir = "data/stimuli_toy_x_1/participant_instructions_images_toy_x_1/"
+output_top_dir = "data/stimuli_toy_x_1/"
